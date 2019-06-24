3246052c401974b94d742980035b6e9edac85b05
GuoLei <83286660@qq.com>
2019-06-24 15:34:46 +0800

    增加调试菜单

---
+++ src/demo/Form.vue
@@ -0,0 +1,47 @@
+<template>
+  <fm-generate-form
+    :data="jsonData"
+    :remote="remoteFuncs"
+    :value="values"
+    ref="generateForm">
+
+    <template v-slot:blank="scope">
+              宽度：<el-input v-model="scope.model.blank.width" style="width: 100px"></el-input>
+              高度：<el-input v-model="scope.model.blank.height" style="width: 100px"></el-input>
+            </template>
+  </fm-generate-form>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      jsonData: {
+        "list": [
+    {
+      "type": "blank",
+      "name": "自定义",
+      "icon": "icon-ic",
+      "options": {
+        "defaultType": "String",
+        "remoteFunc": "func_1561356971000_97592"
+      },
+      "key": "1561356971000_97592",
+      "model": "blank",
+      "rules": []
+    }
+  ],
+  "config": {
+    "labelWidth": 100,
+    "labelPosition": "top",
+    "size": "small"
+  }
+      },
+          editData: {},
+          remoteFuncs: {
+            
+          }
+    }
+  }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ export default new Router({
       component: Home
     },
     {
+      path: '/form',
+      name: 'form',
+      component: () => import('../demo/Form.vue')
+    },
+    {
       path: '/price',
       name: 'price',
       component: () => import('../demo/Price.vue')
