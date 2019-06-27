2cb009d2d464ef67e6d4d13510dfe607925f4229
GuoLei <83286660@qq.com>
2019-06-27 16:23:03 +0800

    fix: preview form

--- src/demo/Form.vue
+++ src/demo/Form.vue
@@ -12,310 +12,155 @@ export default {
   data () {
     return {
       jsonData: {
-        "list": [
+  "list": [
     {
-      "type": "grid",
-      "name": "栅格布局",
-      "icon": "icon-grid-",
-      "columns": [
-        {
-          "span": 8,
-          "list": []
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "blank",
-              "name": "标题",
-              "icon": "icon-ic",
-              "options": {
-                "defaultType": "String",
-                "remoteFunc": "func_1561363869000_92637"
-              },
-              "key": "1561363869000_92637",
-              "model": "blank_1561363869000_92637",
-              "rules": []
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "blank",
-              "name": "评判",
-              "icon": "icon-ic",
-              "options": {
-                "defaultType": "String",
-                "remoteFunc": "func_1561363884000_80894"
-              },
-              "key": "1561363884000_80894",
-              "model": "blank_1561363884000_80894",
-              "rules": []
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "blank",
-              "name": "范围",
-              "icon": "icon-ic",
-              "options": {
-                "defaultType": "String",
-                "remoteFunc": "func_1561363894000_58278"
-              },
-              "key": "1561363894000_58278",
-              "model": "blank_1561363894000_58278",
-              "rules": []
-            }
-          ]
-        }
-      ],
+      "type": "input",
+      "name": "单行文本",
+      "icon": "icon-input",
+      "cols": 12,
       "options": {
-        "gutter": 36,
-        "justify": "end",
-        "align": "middle",
-        "remoteFunc": "func_1561363845000_83509"
+        "width": "100%",
+        "defaultValue": "",
+        "required": false,
+        "dataType": "string",
+        "pattern": "",
+        "placeholder": "",
+        "remoteFunc": "func_1561623027000_24735"
       },
-      "key": "1561363845000_83509",
-      "model": "grid_1561363845000_83509",
+      "key": "1561623027000_24735",
+      "model": "input_1561623027000_24735",
+      "rules": [
+        {
+          "type": "string",
+          "message": "单行文本格式不正确"
+        }
+      ]
+    },
+    {
+      "type": "textarea",
+      "name": "多行文本",
+      "icon": "icon-diy-com-textarea",
+      "cols": 12,
+      "options": {
+        "width": "100%",
+        "defaultValue": "",
+        "required": false,
+        "disabled": false,
+        "pattern": "",
+        "placeholder": "",
+        "remoteFunc": "func_1561623029000_95127"
+      },
+      "key": "1561623029000_95127",
+      "model": "textarea_1561623029000_95127",
       "rules": []
     },
     {
-      "type": "grid",
-      "name": "栅格布局",
-      "icon": "icon-grid-",
-      "columns": [
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "blank",
-              "name": "高血压",
-              "icon": "icon-ic",
-              "options": {
-                "defaultType": "String",
-                "remoteFunc": "func_1561364239000_52046"
-              },
-              "key": "1561364239000_52046",
-              "model": "blank_1561364239000_52046",
-              "rules": []
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "input",
-              "name": "11订单",
-              "icon": "icon-input",
-              "options": {
-                "width": "100%",
-                "defaultValue": "",
-                "required": false,
-                "dataType": "string",
-                "pattern": "",
-                "placeholder": "",
-                "remoteFunc": "func_1561364223000_6671"
-              },
-              "key": "1561364223000_6671",
-              "model": "input_1561364223000_6671",
-              "rules": [
-                {
-                  "type": "string",
-                  "message": "11订单格式不正确"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "input",
-              "name": "",
-              "icon": "icon-input",
-              "options": {
-                "width": "100%",
-                "defaultValue": "",
-                "required": false,
-                "dataType": "string",
-                "pattern": "",
-                "placeholder": "",
-                "remoteFunc": "func_1561364249000_51440"
-              },
-              "key": "1561364249000_51440",
-              "model": "input_1561364249000_51440",
-              "rules": [
-                {
-                  "type": "string",
-                  "message": "格式不正确"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "input",
-              "name": "",
-              "icon": "icon-input",
-              "options": {
-                "width": "100%",
-                "defaultValue": "",
-                "required": false,
-                "dataType": "string",
-                "pattern": "",
-                "placeholder": "",
-                "remoteFunc": "func_1561364258000_1470"
-              },
-              "key": "1561364258000_1470",
-              "model": "input_1561364258000_1470",
-              "rules": [
-                {
-                  "type": "string",
-                  "message": "格式不正确"
-                }
-              ]
-            }
-          ]
-        }
-      ],
+      "type": "input",
+      "name": "单行文本",
+      "icon": "icon-input",
+      "cols": 12,
       "options": {
-        "gutter": 0,
-        "justify": "center",
-        "align": "top",
-        "remoteFunc": "func_1561364201000_12486"
+        "width": "100%",
+        "defaultValue": "",
+        "required": false,
+        "dataType": "string",
+        "pattern": "",
+        "placeholder": "",
+        "remoteFunc": "func_1561623032000_98125"
       },
-      "key": "1561364201000_12486",
-      "model": "grid_1561364201000_12486",
+      "key": "1561623032000_98125",
+      "model": "input_1561623032000_98125",
+      "rules": [
+        {
+          "type": "string",
+          "message": "单行文本格式不正确"
+        }
+      ]
+    },
+    {
+      "type": "radio",
+      "name": "单选框组",
+      "icon": "icon-radio-active",
+      "cols": 12,
+      "options": {
+        "inline": false,
+        "defaultValue": "",
+        "showLabel": false,
+        "options": [
+          {
+            "value": "选项1",
+            "label": "选项1"
+          },
+          {
+            "value": "选项2",
+            "label": "选项2"
+          },
+          {
+            "value": "选项3",
+            "label": "选项3"
+          }
+        ],
+        "required": false,
+        "width": "",
+        "remote": false,
+        "remoteOptions": [],
+        "props": {
+          "value": "value",
+          "label": "label"
+        },
+        "remoteFunc": "func_1561623035000_50587"
+      },
+      "key": "1561623035000_50587",
+      "model": "radio_1561623035000_50587",
       "rules": []
     },
     {
-      "type": "grid",
-      "name": "栅格布局",
-      "icon": "icon-grid-",
-      "columns": [
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "blank",
-              "name": "糖尿病",
-              "icon": "icon-ic",
-              "options": {
-                "defaultType": "String",
-                "remoteFunc": "func_1561366187000_43394"
-              },
-              "key": "1561366187000_43394",
-              "model": "blank_1561366187000_43394",
-              "rules": []
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "input",
-              "name": "",
-              "icon": "icon-input",
-              "options": {
-                "width": "100%",
-                "defaultValue": "",
-                "required": false,
-                "dataType": "string",
-                "pattern": "",
-                "placeholder": "",
-                "remoteFunc": "func_1561366195000_69922"
-              },
-              "key": "1561366195000_69922",
-              "model": "input_1561366195000_69922",
-              "rules": [
-                {
-                  "type": "string",
-                  "message": "格式不正确"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "input",
-              "name": "",
-              "icon": "icon-input",
-              "options": {
-                "width": "100%",
-                "defaultValue": "",
-                "required": false,
-                "dataType": "string",
-                "pattern": "",
-                "placeholder": "",
-                "remoteFunc": "func_1561366198000_33663"
-              },
-              "key": "1561366198000_33663",
-              "model": "input_1561366198000_33663",
-              "rules": [
-                {
-                  "type": "string",
-                  "message": "格式不正确"
-                }
-              ]
-            }
-          ]
-        },
-        {
-          "span": 8,
-          "list": [
-            {
-              "type": "input",
-              "name": "",
-              "icon": "icon-input",
-              "options": {
-                "width": "100%",
-                "defaultValue": "",
-                "required": false,
-                "dataType": "string",
-                "pattern": "",
-                "placeholder": "",
-                "remoteFunc": "func_1561366201000_62124"
-              },
-              "key": "1561366201000_62124",
-              "model": "input_1561366201000_62124",
-              "rules": [
-                {
-                  "type": "string",
-                  "message": "格式不正确"
-                }
-              ]
-            }
-          ]
-        }
-      ],
+      "type": "input",
+      "name": "单行文本",
+      "icon": "icon-input",
+      "cols": 12,
       "options": {
-        "gutter": 0,
-        "justify": "start",
-        "align": "top",
-        "remoteFunc": "func_1561366161000_25928"
+        "width": "100%",
+        "defaultValue": "",
+        "required": false,
+        "dataType": "string",
+        "pattern": "",
+        "placeholder": "",
+        "remoteFunc": "func_1561623037000_32246"
       },
-      "key": "1561366161000_25928",
-      "model": "grid_1561366161000_25928",
+      "key": "1561623037000_32246",
+      "model": "input_1561623037000_32246",
+      "rules": [
+        {
+          "type": "string",
+          "message": "单行文本格式不正确"
+        }
+      ]
+    },
+    {
+      "type": "number",
+      "name": "计数器",
+      "icon": "icon-number",
+      "cols": 12,
+      "options": {
+        "width": "",
+        "required": false,
+        "defaultValue": 0,
+        "min": 0,
+        "max": 0,
+        "step": 1,
+        "disabled": false,
+        "controlsPosition": "",
+        "remoteFunc": "func_1561623041000_57961"
+      },
+      "key": "1561623041000_57961",
+      "model": "number_1561623041000_57961",
       "rules": []
     }
   ],
   "config": {
     "labelWidth": 100,
-    "labelPosition": "left",
+    "labelPosition": "top",
     "size": "small"
   }
       },
