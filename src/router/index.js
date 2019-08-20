d207823c1a39d76c521e74991cb279cb145c57ae
GuoLei <83286660@qq.com>
2019-08-20 16:20:41 +0800

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Home from '../demo/Home.vue'
+import Form from '../demo/Form.vue'
 
 Vue.use(Router)
 
@@ -14,7 +15,7 @@ export default new Router({
     {
       path: '/form',
       name: 'form',
-      component: () => import('../demo/Form.vue')
+      component: Form
     }
   ]
 })
